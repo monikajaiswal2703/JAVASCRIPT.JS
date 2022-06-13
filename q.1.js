63d566f39429a645135fd8ee863519e05456ab5d
Monika Jaiswal <92169645+monikajaiswal2703@users.noreply.github.com>
2022-06-13 22:50:38 +0530

    Add files via upload

---
+++ q.1.js
@@ -0,0 +1,25 @@
+// var myFavouriteThings = ["Ramani", "Black",1, "jan 2 1996"];
+// console.log(typeof(myFavouriteThings))
+// console.log(myFavouriteThings)
+
+// var myFavouriteFriends = ["shweta", "rani", "nayak", "kajal"]
+// console.log(myFavouriteFriends)
+
+// var fruits = ['Apple', 'Banana','orange'];
+// console.log(fruits.length); 
+// output 2
+
+// var fruits = ['Apple', 'Banana', 'Orange']
+// console.log(fruits[2]); // output ‘Banana’
+
+
+var array = [ 1, 2, 3, 4, 5, 6 ];
+for (var index = 0; index < array; index++) {
+console.log(array[index]);
+}
+//output
+// 1
+// 2
+// 3
+// 4
+// 5
